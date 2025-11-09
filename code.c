6d65dbbe17f91203f4bacebe82d9476a19fea6ed
guerreirof25-crypto <guerreirof25@gmail.com>
2025-11-09 20:52:23 -0300

    resetei para o codigo anterior
    fui tentar otimizar as funções auxiliares e fiz errado, copiei o codigo antigo de volta para cá

--- code.c
+++ code.c
@@ -5,27 +5,48 @@
 #define ARQ "veiculos.dat"
 #define ARQ_TEMP "temp.dat"
 
+typedef enum{CARRO=1,MOTO,CAMINHAO} Tipo;
+
+typedef struct{
+  int id;
+  int removido;
+  Tipo tipo;
+  char nome[40];
+  union{
+    struct{int portas;int manut[3][2];} carro;
+    struct{int cilindradas;} moto;
+    struct{float carga;} caminhao;
+  }info;
+}Veiculo;
+
 //////////////////////////////////////////////////////////////////////FUNÇÕES AUXILIARES/////////////////////////////////////////////////////////////////////////////
 
-int lerInt(char *msg) {
+int lerInt(const char *msg) {
     int n;
     printf("%s", msg);
-    scanf("%d", &n);
+    while (scanf("%d", &n) != 1) {
+        printf("Valor inválido, digite novamente: ");
+        while (getchar() != '\n');
+    }
+    while (getchar() != '\n');
     return n;
 }
 
-float lerFloat(char *msg) {
+float lerFloat(const char *msg) {
     float n;
     printf("%s", msg);
-    scanf("%f", &n);
+    while (scanf("%f", &n) != 1) {
+        printf("Valor inválido, digite novamente: ");
+        while (getchar() != '\n');
+    }
+    while (getchar() != '\n');
     return n;
 }
 
-void lerString(char *msg, char *s) {
+void lerString(const char *msg, char *dest, size_t tam) {
     printf("%s", msg);
-    fflush(stdin);
-    fgets(s, 40, stdin);
-    s[strcspn(s, "\n")] = '\0'; // remover \n
+    fgets(dest, tam, stdin);
+    dest[strcspn(dest, "\n")] = 0;
 }
 
 //////////////////////////////////////////////////////////////////////////EDITAR O VEÍCULO//////////////////////////////////////////////////////////////////////////////////////
