701bf83a1e24ad5150c137117f95ee8fba8ecdca
guerreirof25-crypto <guerreirof25@gmail.com>
2025-11-09 20:33:42 -0300

    Refactor input functions in code.c
    Refactor input functions to simplify parameter types and improve readability.

--- code.c
+++ code.c
@@ -5,48 +5,27 @@
 #define ARQ "veiculos.dat"
 #define ARQ_TEMP "temp.dat"
 
-typedef enum{CARRO=1,MOTO,CAMINHAO} Tipo;
-
-typedef struct{
-  int id;
-  int removido;
-  Tipo tipo;
-  char nome[40];
-  union{
-    struct{int portas;int manut[3][2];} carro;
-    struct{int cilindradas;} moto;
-    struct{float carga;} caminhao;
-  }info;
-}Veiculo;
-
 //////////////////////////////////////////////////////////////////////FUNÇÕES AUXILIARES/////////////////////////////////////////////////////////////////////////////
 
-int lerInt(const char *msg) {
+int lerInt(char *msg) {
     int n;
     printf("%s", msg);
-    while (scanf("%d", &n) != 1) {
-        printf("Valor inválido, digite novamente: ");
-        while (getchar() != '\n');
-    }
-    while (getchar() != '\n');
+    scanf("%d", &n);
     return n;
 }
 
-float lerFloat(const char *msg) {
+float lerFloat(char *msg) {
     float n;
     printf("%s", msg);
-    while (scanf("%f", &n) != 1) {
-        printf("Valor inválido, digite novamente: ");
-        while (getchar() != '\n');
-    }
-    while (getchar() != '\n');
+    scanf("%f", &n);
     return n;
 }
 
-void lerString(const char *msg, char *dest, size_t tam) {
+void lerString(char *msg, char *s) {
     printf("%s", msg);
-    fgets(dest, tam, stdin);
-    dest[strcspn(dest, "\n")] = 0;
+    fflush(stdin);
+    fgets(s, 40, stdin);
+    s[strcspn(s, "\n")] = '\0'; // remover \n
 }
 
 //////////////////////////////////////////////////////////////////////////EDITAR O VEÍCULO//////////////////////////////////////////////////////////////////////////////////////
