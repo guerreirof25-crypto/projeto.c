a2cd7f3ddd74d029cf0d19685e522df12198fecb
guerreirof25-crypto <guerreirof25@gmail.com>
2025-11-09 21:00:59 -0300

    Remove Veiculo struct and Tipo enum from code.c
    Removed the Veiculo struct and Tipo enum definitions.

--- code.c
+++ code.c
@@ -1,23 +1,11 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+#include "veiculos.h" 
+
 
 #define ARQ "veiculos.dat"
 #define ARQ_TEMP "temp.dat"
-
-typedef enum{CARRO=1,MOTO,CAMINHAO} Tipo;
-
-typedef struct{
-  int id;
-  int removido;
-  Tipo tipo;
-  char nome[40];
-  union{
-    struct{int portas;int manut[3][2];} carro;
-    struct{int cilindradas;} moto;
-    struct{float carga;} caminhao;
-  }info;
-}Veiculo;
 
 //////////////////////////////////////////////////////////////////////FUNÇÕES AUXILIARES/////////////////////////////////////////////////////////////////////////////
 
